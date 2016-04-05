169567ff9a5868fddef89dffca650b7b858ad53a
Emilio Cobos Álvarez <ecoal95@gmail.com>
2016-04-05 18:44:58 +0200

    bench: Generate time/workers graphics

--- bench/gen-graphics.py
+++ bench/gen-graphics.py
@@ -2,6 +2,7 @@ import sys
 import os
 import re
 import configparser
+import collections
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -21,6 +22,7 @@ def get_metadata_from_filename(name):
   assert basename.pop(0) == 'job'
   assert basename.pop(0) == 'size'
   ret['job_size'] = basename.pop(0)
+  ret['password_hash'] = basename.pop(0)
 
   return ret
 
@@ -41,15 +43,12 @@ def humanize(snake_cased):
   return s
 
 
-def gen_graph_for_file(f, destination, metadata):
+# Generate a plot for a file, and returns each iteration information
+# in a dictionary
+def gen_graph_for_file(f, destination, description_meta):
   header = None
 
   fig = plt.figure()
-  description_meta = get_metadata_from_filename(destination)
-  description_meta['processor_name'] = metadata.get('processor', 'name');
-  description_meta['processor_threads'] = metadata.get('processor', 'threads');
-  description_meta['processor_frequency'] = metadata.get('processor', 'frequency');
-
   plt.gca().set_position((.1, .4, .8, .5)) # to make a bit of room for extra text
 
   description = ["{}: {}".format(humanize(key), value) for key, value in description_meta.items()]
@@ -59,6 +58,8 @@ def gen_graph_for_file(f, destination, metadata):
   plt.xlabel('Worker')
   plt.ylabel('Iterations')
   workers = 0
+
+  rounds_info = list()
 
   for line in f:
     if not header:
@@ -70,9 +71,20 @@ def gen_graph_for_file(f, destination, metadata):
       continue
 
     data = list(map(str.strip, line.split(',')))
-    label = "{} ({}): {}s".format(unquote(data.pop(0)),
-                                  unquote(data.pop(0)),
-                                  unquote(data.pop(0)))
+    password = unquote(data.pop(0))
+    decrypted = unquote(data.pop(0))
+    time = unquote(data.pop(0))
+
+    label = "{} ({}): {}s".format(password,
+                                  decrypted,
+                                  time)
+
+    rounds_info.append({
+      'password': password,
+      'decrypted': decrypted,
+      'time': float(time)
+    })
+
     # Plot the lines and the points
     plt.plot(range(0, workers), list(map(int, data)), marker='o', label=label)
 
@@ -82,21 +94,86 @@ def gen_graph_for_file(f, destination, metadata):
                     ncol=1, fancybox=True, prop=font_props)
   plt.ylim(ymin=0)
   plt.savefig(destination)
+  plt.close()
+
+  return rounds_info
 
 def gen_graphs_for_dir(directory):
+  cases_by_passwords = collections.defaultdict(
+                         lambda: collections.defaultdict(
+                           lambda: collections.defaultdict(
+                             lambda: collections.defaultdict(dict))))
+
   for subdir, dirs, files in os.walk(directory):
     for file in files:
       if not file.endswith('.csv'):
         continue
 
-      metadata = configparser.ConfigParser()
-      metadata.read(os.path.join(subdir, 'metadata.ini'))
+      config = configparser.ConfigParser()
+      config.read(os.path.join(subdir, 'metadata.ini'))
+      metadata = get_metadata_from_filename(file)
+      metadata['processor_name'] = config.get('processor', 'name')
+      metadata['processor_threads'] = config.get('processor', 'threads')
+      metadata['processor_frequency'] = config.get('processor', 'frequency')
 
       path = os.path.join(subdir, file)
+      rounds_info = None
       with open(path) as f:
-        gen_graph_for_file(f, path + '.svg', metadata)
+        rounds_info = gen_graph_for_file(f, path + '.svg', metadata)
 
-      print(path)
+      assert rounds_info is not None
+      metadata['rounds'] = rounds_info
+      passwords = ''.join(map(lambda r: r['password'], rounds_info)).replace('/', '_')
+      cases_by_passwords[passwords][metadata['compilation_mode']][metadata['running_mode']][metadata['job_size']][metadata['workers']] = metadata
+
+      print(file)
+
+  gen_graphs_by_password(directory, cases_by_passwords)
+
+def gen_graphs_by_password(directory, cases_by_password):
+  out_dir = os.path.join(directory, 'by-password')
+
+  if not os.path.isdir(out_dir):
+    os.makedirs(out_dir)
+
+  for password_hash, case in cases_by_password.items():
+    for compilation_mode, case in case.items():
+      for running_mode, case in case.items():
+        for job_size, case in case.items():
+          out_file = os.path.join(out_dir, "{}-{}-job-size-{}-{}.svg".format(compilation_mode,
+                                                                             running_mode,
+                                                                             job_size,
+                                                                             password_hash))
+          gen_graph_for_case(out_file, case)
+
+def gen_graph_for_case(out_file, case):
+  fig = plt.figure()
+  points = []
+
+  cases = 0
+  for workers, metadata in case.items():
+    cases += 1
+    total_time = 0
+    assert workers == metadata['workers']
+    for round_info in metadata['rounds']:
+      total_time += round_info['time']
+
+    points.append((int(workers), total_time))
+
+  # Sort by x
+  points.sort(key=lambda p: p[0])
+  print(points)
+
+  x = list(map(lambda p: p[0], points))
+  y = list(map(lambda p: p[1], points))
+
+  plt.xlabel('Number of workers')
+  plt.ylabel('Computation time')
+  plt.plot(x, y, marker='o')
+  plt.ylim(ymin=0)
+  plt.savefig(out_file)
+
+  plt.close()
 
 def main():
   gen_graphs_for_dir(BENCH_DIR)
